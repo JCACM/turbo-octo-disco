e0f68192bd7fc920ab57826de4fec85b649636e7
JCACM <jonathan.aiwazian@gmail.com>
2016-10-13 20:31:19 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            int number1 = 1;
+            int number2 = 2;
+            int number3 = number1 + number2;
+            Console.WriteLine(number3);
+        }
+    }
+}
